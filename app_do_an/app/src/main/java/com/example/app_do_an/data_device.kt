9483f60283b6e1fc0e6f02c002ca9626f4120810
mieuhong08032002 <hongmieu2002@gmail.com>
2024-10-26 22:33:42 +0700

    push

---
+++ app_do_an/app/src/main/java/com/example/app_do_an/data_device.kt
@@ -0,0 +1,7 @@
+package com.example.app_do_an
+
+class data_device( var dv1 : Int? = null,
+                   var dv2 : Int? = null,
+                   var dv3 : Int? = null,
+                   var dv4: Int? = null) {
+}
